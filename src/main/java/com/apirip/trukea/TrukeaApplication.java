95a15599d083c615576689ce7e321d9b764cc472
gianf <gianfranco.pupiales@epn.edu.ec>
2025-11-23 21:58:46 -0500

    add authentication success handler and signup form validation

--- src/main/java/com/apirip/trukea/TrukeaApplication.java
+++ src/main/java/com/apirip/trukea/TrukeaApplication.java
@@ -2,8 +2,10 @@ package com.apirip.trukea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TrukeaApplication {
 
     public static void main(String[] args) {
