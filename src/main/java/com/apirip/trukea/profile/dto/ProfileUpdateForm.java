ac667b5937e39b70bc59a0d5e11a344a36bdf602
reneguzman7 <reneguzman7@outlook.com>
2025-11-23 22:15:05 -0500

    feat: Add profile management features, including a DTO for updates and a controller for viewing and editing user profiles.

---
+++ src/main/java/com/apirip/trukea/profile/dto/ProfileUpdateForm.java
@@ -0,0 +1,13 @@
+package com.apirip.trukea.profile.dto;
+
+import jakarta.validation.constraints.*;
+import org.springframework.web.multipart.MultipartFile;
+
+public record ProfileUpdateForm(
+        @NotBlank @Email(regexp = "^[a-zA-Z]+(\\.[a-zA-Z]+[0-9]*)?@epn\\.edu\\.ec$") String email,
+        String password,                      // opcional; si viene vacío se conserva
+        @NotBlank String name,
+        @Pattern(regexp="\\d{10}") String phone,
+        String existingPhoto,                 // para conservar la foto actual
+        MultipartFile newPhoto                // opcional
+) {}
