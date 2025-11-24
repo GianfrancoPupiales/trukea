d0b99b07038ff8f713d8e31b3279065ed5739088
gianf <gianfranco.pupiales@epn.edu.ec>
2025-11-23 21:23:59 -0500

    initialize project structure with Maven and Spring Boot setup

---
+++ src/main/java/com/apirip/trukea/TrukeaApplication.java
@@ -0,0 +1,13 @@
+package com.apirip.trukea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrukeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrukeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apirip/trukea/TrukeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirip.trukea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrukeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
